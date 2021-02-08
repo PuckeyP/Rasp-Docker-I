7ba1b4a46a8c6bb356ad6cd40dea04a5245f6a97
PPee <puckeyp@gmail.com>
2021-02-08 12:39:07 +0100

    commit all

--- BashFiles/PiHole.sh
+++ BashFiles/PiHole.sh
@@ -1,17 +1,17 @@
-sudo docker network create \
-            --driver=macvlan \
-            --gateway=192.168.1.1 \
-            --subnet=192.168.1.0/24 \
-            --ip-range=192.168.1.4/32 \
-            -o parent=eth0 VLAN1_pi-hole
+# sudo docker network create \
+#             --driver=macvlan \
+#             --gateway=192.168.1.1 \
+#             --subnet=192.168.1.0/24 \
+#             --ip-range=192.168.1.4/32 \
+#             -o parent=eth0 VLAN1_pi-hole
 
 
 sudo docker run --detach \
            --name pi-hole \
            --restart always \
            --volume /etc/localtime:/etc/localtime:ro \
-           --volume /volume1/docker/PiHole/config:/etc/pihole \
-           --volume /volume1/docker/PiHole/dnsmasq.d:/etc/dnsmasq.d \
+           --volume /home/pi/Rasp-Docker-I/PiHole/config:/etc/pihole \
+           --volume /home/pi/Rasp-Docker-I/PiHole/dnsmasq.d:/etc/dnsmasq.d \
            --cap-add NET_ADMIN \
            --dns=8.8.8.8 \
            --dns=8.8.4.4 \
@@ -24,4 +24,6 @@ sudo docker run --detach \
            --network VLAN1_pi-hole \
            --ip "192.168.1.4" \
            --mac-address "04:44:c0:a4:04:a4" \
-           pihole/pihole:latest
+           pihole@sha256:27f75488f8971127859cd7a81cd4f63ac3d3e01ae128fd0630a47379c9fa1770
+
+#           pihole/pihole:dev-buster
--- BashFiles/Unifi.sh
+++ BashFiles/Unifi.sh
@@ -1,13 +1,16 @@
 # mkdir -p unifi/data
 # mkdir -p unifi/log
-sudo docker run --detach \
+sudo docker run --init \
+                --detach \
                 --name Unifi \
                 --restart always \
-                # --volume /home/pi/Rasp-Docker-I/Unifi:/unifi \
-                --init \
+                --volume /home/pi/Rasp-Docker-I/Unifi/log:/unifi/log \
+                --volume /home/pi/Rasp-Docker-I/Unifi/data:/unifi/data \
                 -p 8080:8080 \
                 -p 8443:8443 \
                 -p 3478:3478 \
                 --env "TZ=Europe/Amsterdam" \
-                jacobalberty/unifi:stable
+                jacobalberty/unifi:6.0
+ 
+
  
